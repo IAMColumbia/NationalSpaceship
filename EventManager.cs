40053bcadd2c9de1b6023a487aabd61c6661d28c
Noah M. Johnson <noahmjohnson@gmail.com>
2015-03-10 20:03:53 -0500

    Update EventManager.cs

--- EventManager.cs
+++ EventManager.cs
@@ -42,29 +42,3 @@ public class EventManager : MonoBehaviour
 	}
 
 }
-
-//public class DelegateScript : MonoBehaviour
-//{
-//    delegate void MyDelegate(int num);
-//    MyDelegate myDelegate;
-
-
-//    void Start()
-//    {
-//        myDelegate = PrintNum;
-//        myDelegate(50);
-
-//        myDelegate = DoubleNum;
-//        myDelegate(50);
-//    }
-
-//    void PrintNum(int num)
-//    {
-//        print("Print Num: " + num);
-//    }
-
-//    void DoubleNum(int num)
-//    {
-//        print("Double Num: " + num * 2);
-//    }
-//}
